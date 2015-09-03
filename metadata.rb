40a680c2649ca452c64d02fb486152b927f13f94
ajmorris <andrew.morris5@alaskaair.com>
2015-09-03 11:25:16 -0700

    lols

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'sample-cookbook'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures sample-cookbook'
+long_description 'Installs/Configures sample-cookbook'
+version          '0.1.0'
+
